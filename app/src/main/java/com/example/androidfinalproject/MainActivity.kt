fa29bfc30422344a1ee012baa1e3a6c752455eed
Danit <danit120@gmail.com>
2022-05-15 12:21:05 +0300

    Hi this is Danit !!

--- app/src/main/java/com/example/androidfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/androidfinalproject/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.androidfinalproject
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.title = ""
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#000000")))
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+       menuInflater.inflate(R.menu.nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       when(item.itemId){
+
+
+
+       }
+        return super.onOptionsItemSelected(item)
     }
 }
